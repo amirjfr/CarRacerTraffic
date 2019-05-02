4ab40120db33a49df2b2fe3a63b0a6d916fc6347
Amir <amir.jafarvand@gmail.com>
2019-05-02 11:14:57 -0700

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private Vector2 targetPos;
+    public float increment;
+    public float speed;
+    public float maxLength;
+    public float minLength;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minLength )
+        {
+            targetPos = new Vector2(transform.position.x - increment , transform.position.y);
+
+        } else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxLength)
+        {
+            targetPos = new Vector2(transform.position.x + increment, transform.position.y);
+
+        }
+
+    }
+}
